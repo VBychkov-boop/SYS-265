0b8a8ff0e59435645f48bdd4e21b200b4b08bdd8
VBychkov-boop <creativedogs101@gmail.com>
2026-02-16 23:29:53 +0100

    added secure-ssh script

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#securessh.sh
+#author VBychkov-boop
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
